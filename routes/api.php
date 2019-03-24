a70095792dcc5e472ac2feeda2bae8f771e159b6
Ardi83 <mohammad.nasirabadi@chasacademy.se>
2019-03-24 05:31:03 +0100

    passport removed , routes and controllers for login signup resetpass responsepass jwt added

--- routes/api.php
+++ routes/api.php
@@ -2,80 +2,45 @@
  
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::resource('recipes', 'RecipeController')->middleware('cors');
-
 Route::group([
-    'middleware' => 'cors',
-    'prefix' => 'recipes'], function(){
-    Route::apiResource('/{recipe}/comments', 'CommentController')->only([
-        'index'
-    ])->middleware('cors');
+    'middleware' => 'api',
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('sendPasswordResetLink', 'ResetPasswordController@sendEmail');
+    Route::post('resetPassword', 'ChangePasswordController@process');
 });
 
-Route::group([
-    'middleware' => 'cors',
-    'prefix' => 'recipes'], function(){
+
+
+Route::resource('recipes', 'RecipeController');
+
+Route::group(['prefix' => 'recipes'], function(){
+    Route::apiResource('/{recipe}/comments', 'CommentController')->only([
+        'index'
+    ]);
+});
+
+Route::group(['prefix' => 'recipes'], function(){
     Route::apiResource('/{recipe}/meal', 'MealController')->only([
         'index'
     ]);
 });
 
-Route::group([
-    'middleware' => 'cors',
-    'prefix' => 'recipes'], function(){
+Route::group(['prefix' => 'recipes'], function(){
     Route::apiResource('/{recipe}/ingredients', 'IngredientController');
 });
 
 Route::get('meals', [
     'uses' => 'MealsController@index',
     'as' => 'Meals'
-])->middleware('cors');
+]);
 
 Route::get('ingredients', [
     'uses' => 'IngredientsController@index',
     'as' => 'Ingredients'
-])->middleware('cors');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Route::get(
-//     'recipes/{recipe}/relationship/meal',
-//     [
-//         'uses' => \App\Http\Controllers\RecipeRelationshipController::class . '@meal',
-//         'as' => 'recipes.relationship.meal',
-//     ]
-//     );
-
-// Route::get(
-//     'recipes/{recipe}/meal',
-//     [
-//         'uses' => \App\Http\Controllers\RecipeRelationshipController::class . '@meal',
-//         'as' => 'recipes.meal',
-//     ]
-//     );
+]);
